e47e51b134c5329b2cc8bad7af7de070b6705869
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-03 19:05:41 -0700

    created the models in the index and the the catergory js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -6,11 +6,12 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      port: 3306,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-      port: 3306
+
     });
 
 module.exports = sequelize;
