263db19096f9e8dcda2abfe3bfd76b184a08a13b
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-03 19:21:22 -0700

    Tag model built

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize = process.env.JAWSDB_URL
+  if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+  } else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       port: 3306,
       dialect: 'mysql',
@@ -13,5 +15,5 @@ const sequelize = process.env.JAWSDB_URL
       },
 
     });
-
+  }
 module.exports = sequelize;
