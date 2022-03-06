395c24d131501ae9d004008cec64a828f6a50cb3
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-05 18:50:38 -0700

    added the ability to find one or the ability to create a new catergory

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -31,7 +31,31 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [{
   // be sure to include its associated Products
+    model: Product,
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+  }
+  ]
+  })
+  .then(categoryData => {
+  if (!categoryData) {
+    res.status(404).json({ message: "No category found with this id" });
+    return;
+  }
+  res.json(categoryData);
+  })
+  .catch(err => {
+  res.status(500).json({ message: "There has been an error" })
+  });
 });
 
 router.post('/', (req, res) => {
