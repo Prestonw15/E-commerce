c620a8718f31efc8bb6114bfa77fddea9fe72a13
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-05 19:14:07 -0700

    added the create tag find all tags, and find single tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -5,12 +5,58 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
   // be sure to include its associated Product data
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  ]
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "No data found" });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "There has been an error "})
+  });
+
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'tag_name'
+    ],
   // be sure to include its associated Product data
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  ]
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({ message: "No tag with that id found" });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "There has been an error "})
+  });
 });
 
 router.post('/', (req, res) => {
